37c3f1d8a86647b5f5c3859705fa187a2cba5cae
SHOTA <showsky.pac33007@gmail.com>
2021-10-04 13:55:21 +0900

    CommentLike_table

--- api/topicsLab/database/migrations/2021_10_04_045441_create_comment_likes_table.php
+++ api/topicsLab/database/migrations/2021_10_04_045441_create_comment_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTopicLikesTable extends Migration
+class CreateCommentLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTopicLikesTable extends Migration
      */
     public function up()
     {
-        Schema::create('topic_likes', function(Blueprint $table) {
+        Schema::create('comment_likes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateTopicLikesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('topic_likes');
+        Schema::dropIfExists('comment_likes');
     }
 }
