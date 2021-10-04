39d5ad12a357b6c2d7a6caad0feb9b18487b3b19
Shunya Tanaka <soccerplayer161236@gmail.com>
2021-10-04 16:40:17 +0900

    10/4_change

--- front/topicslab/src/App.vue
+++ front/topicslab/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">ホーム</router-link> |
     <template v-if="authenticated">
-      <a>mypage</a>
+      <router-link to="/mypage">マイページ</router-link> |
     </template>
     <template v-else>
-      <router-link to="/login">login</router-link>
+      <router-link to="/login">ログイン</router-link> |
     </template>
   </div>
   <div class="content">
     <router-view/>
   </div>
+  <p><small>&copy; 2021 topics lab</small></p>
 </template>
 
 <script>
@@ -67,4 +68,9 @@ body {
     }
   }
 }
+
+p{
+  text-align: center;
+}
+
 </style>
